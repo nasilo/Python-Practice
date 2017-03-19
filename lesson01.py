302e4f558ee8c73251ad7a147559e01f9d19a2e9
nasilo <nick.a.asilo@gmail.com>
2017-03-19 17:42:09 -0400

    Lesson 1

---
+++ lesson01.py
@@ -0,0 +1,29 @@
+my_int = 7
+my_float = 1.23
+my_bool = True
+
+def spam():
+    eggs = 12
+    return eggs
+
+print spam()
+
+# Comment Test
+
+"""
+I can apparently use triple quotations to write multi-line comments.
+That is very useful, I don't know how to do that in Ruby.
+I currently wonder if I can do that in Ruby, will look that up later.
+"""
+
+#Set eggs equal to 100 using exponentiation on line 3!
+
+eggs = 10 ** 2
+
+print eggs
+
+#This is a comment
+
+monty = True
+python = 1.234
+monty_python = python ** 2
